0b6acd042acffab5d7ead00014ec08ae736c8036
fractalbarista <fractalbarista@gmail.com>
2024-09-29 13:42:27 +0900

    separated TrainerBattleCommand into TrainerBattleCommand and TrainerBattleFlatCommand

--- src/main/java/kiwiapollo/cobblemontrainerbattle/commands/TrainerBattleFlatCommand.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/commands/TrainerBattleFlatCommand.java
@@ -7,14 +7,14 @@ import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import kiwiapollo.cobblemontrainerbattle.trainerbattle.TrainerBattle;
 import net.minecraft.server.command.ServerCommandSource;
 
-public class TrainerBattleCommand extends LiteralArgumentBuilder<ServerCommandSource> {
-    public TrainerBattleCommand() {
-        super("trainerbattle");
+public class TrainerBattleFlatCommand extends LiteralArgumentBuilder<ServerCommandSource> {
+    public TrainerBattleFlatCommand() {
+        super("trainerbattleflat");
 
         this.requires(new PlayerCommandPredicate(getLiteral()))
                 .then(RequiredArgumentBuilder.<ServerCommandSource, String>argument("trainer", StringArgumentType.string())
                         .executes(context -> {
-                            TrainerBattle.battleWithStatusQuo(context, StringArgumentType.getString(context, "trainer"));
+                            TrainerBattle.battleWithFlatLevelAndFullHealth(context, StringArgumentType.getString(context, "trainer"));
                             return Command.SINGLE_SUCCESS;
                         }));
     }
