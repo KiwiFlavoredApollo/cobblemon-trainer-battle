9a8c1012243f7347b038dda79d147729be21a54c
fractalbarista <fractalbarista@gmail.com>
2024-09-29 15:26:51 +0900

    fixed bug traded Pokemon cannot be used due to being fainted

--- src/main/java/kiwiapollo/cobblemontrainerbattle/battleactors/player/BattleFrontierPlayerBattleActorFactory.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/battleactors/player/BattleFrontierPlayerBattleActorFactory.java
@@ -3,6 +3,7 @@ package kiwiapollo.cobblemontrainerbattle.battleactors.player;
 import com.cobblemon.mod.common.api.battles.model.actor.BattleActor;
 import com.cobblemon.mod.common.battles.actor.PlayerBattleActor;
 import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import kiwiapollo.cobblemontrainerbattle.battlefrontier.BattleFrontier;
 import kiwiapollo.cobblemontrainerbattle.battlefrontier.BattleFrontierSession;
 import kotlin.Unit;
@@ -11,6 +12,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 public class BattleFrontierPlayerBattleActorFactory {
     public BattleActor create(ServerPlayerEntity player) {
         BattleFrontierSession session = BattleFrontier.SESSIONS.get(player.getUuid());
+        session.partyPokemons.forEach(Pokemon::heal);
 
         return new PlayerBattleActor(
                 player.getUuid(),
