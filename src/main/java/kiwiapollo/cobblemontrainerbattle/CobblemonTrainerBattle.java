3bfb1faae08883a179488a880cd01f99a219c121
fractalbarista <fractalbarista@gmail.com>
2024-10-07 00:34:28 +0900

    added trainer spawn egg and translation

--- src/main/java/kiwiapollo/cobblemontrainerbattle/CobblemonTrainerBattle.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/CobblemonTrainerBattle.java
@@ -21,11 +21,16 @@ import kotlin.Unit;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.resource.ResourceType;
@@ -51,6 +56,13 @@ public class CobblemonTrainerBattle implements ModInitializer {
 					.setDimensions(0.6f, 1.8f)
 					.build("trainer");
 
+	public static final Item TRAINER_SPAWN_EGG = new SpawnEggItem(
+			TRAINER_ENTITY_TYPE,
+			0xAAAAAA,
+			0xFF5555,
+			new FabricItemSettings().maxCount(64)
+	);
+
 	public static JsonObject defaultTrainerConfiguration = new JsonObject();
 	public static Map<String, TrainerFile> trainerFiles = new HashMap<>();
 	public static Map<String, GroupFile> groupFiles = new HashMap<>();
@@ -58,6 +70,12 @@ public class CobblemonTrainerBattle implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		Registry.register(Registries.ITEM, new Identifier(NAMESPACE, "trainer_spawn_egg"), TRAINER_SPAWN_EGG);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+			entries.add(TRAINER_SPAWN_EGG);
+		});
+
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			dispatcher.register(new TrainerBattleCommand());
 			dispatcher.register(new TrainerBattleFlatCommand());
