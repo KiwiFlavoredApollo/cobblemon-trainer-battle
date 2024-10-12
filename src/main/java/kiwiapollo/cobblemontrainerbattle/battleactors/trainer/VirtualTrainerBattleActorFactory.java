9a03c02b011c7603ae87d5e89dedf171099d73a8
fractalbarista <fractalbarista@gmail.com>
2024-10-12 10:01:41 +0900

    removed deprecated imports

--- src/main/java/kiwiapollo/cobblemontrainerbattle/battleactors/trainer/VirtualTrainerBattleActorFactory.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/battleactors/trainer/VirtualTrainerBattleActorFactory.java
@@ -3,19 +3,13 @@ package kiwiapollo.cobblemontrainerbattle.battleactors.trainer;
 import com.cobblemon.mod.common.api.battles.model.actor.BattleActor;
 import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
 import com.cobblemon.mod.common.pokemon.Pokemon;
-import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
 import kiwiapollo.cobblemontrainerbattle.battleactors.SafeCopyBattlePokemonFactory;
 import kiwiapollo.cobblemontrainerbattle.battleactors.TrainerPokemonFactory;
 import kiwiapollo.cobblemontrainerbattle.battlefactory.BattleFactory;
 import kiwiapollo.cobblemontrainerbattle.common.Generation5AI;
-import kiwiapollo.cobblemontrainerbattle.common.Trainer;
-import kiwiapollo.cobblemontrainerbattle.exceptions.PokemonParseException;
-import kiwiapollo.cobblemontrainerbattle.parsers.SmogonPokemon;
-import kiwiapollo.cobblemontrainerbattle.parsers.SmogonPokemonParser;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
