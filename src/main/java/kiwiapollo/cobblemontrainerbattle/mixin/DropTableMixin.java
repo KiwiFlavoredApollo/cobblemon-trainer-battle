a056d6878a7a35986cf38fcf28a7b90d5ac319af
fractalbarista <fractalbarista@gmail.com>
2024-10-15 15:20:06 +0900

    added @SuppressWarnings annotation

--- src/main/java/kiwiapollo/cobblemontrainerbattle/mixin/DropTableMixin.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/mixin/DropTableMixin.java
@@ -12,8 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashMap;
-
+@SuppressWarnings("unused")
 @Mixin(DropTable.class)
 public class DropTableMixin {
     @Inject(method = "drop", at = @At("HEAD"))
