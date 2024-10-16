6d8f386cddc8d2bf1f4cf7528bf7e6d921e4800b
fractalbarista <fractalbarista@gmail.com>
2024-10-17 02:11:46 +0900

    edited return value type to PartyStore

--- src/main/java/kiwiapollo/cobblemontrainerbattle/battlefactory/RandomPartyFactory.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/battlefactory/RandomPartyFactory.java
@@ -1,15 +1,15 @@
 package kiwiapollo.cobblemontrainerbattle.battlefactory;
 
 import com.cobblemon.mod.common.api.pokemon.PokemonSpecies;
-import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
+import com.cobblemon.mod.common.api.storage.party.PartyStore;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import java.util.Objects;
 
 public class RandomPartyFactory {
-    public static PlayerPartyStore create(ServerPlayerEntity player) {
-        PlayerPartyStore party = new PlayerPartyStore(player.getUuid());
+    public static PartyStore create(ServerPlayerEntity player) {
+        PartyStore party = new PartyStore(player.getUuid());
 
         party.add(PokemonSpecies.INSTANCE.random().create(BattleFactory.LEVEL));
         party.add(PokemonSpecies.INSTANCE.random().create(BattleFactory.LEVEL));
