97196777829207878f6a0eb918b011c35c592b6e
fractalbarista <fractalbarista@gmail.com>
2024-10-17 15:21:19 +0900

    removed deprecated interface

--- src/main/java/kiwiapollo/cobblemontrainerbattle/battleactors/TrainerBattleActorFactory.java
+++
@@ -1,9 +0,0 @@
-package kiwiapollo.cobblemontrainerbattle.battleactors;
-
-import com.cobblemon.mod.common.api.battles.model.actor.BattleActor;
-import net.minecraft.util.Identifier;
-
-public interface TrainerBattleActorFactory {
-    BattleActor createWithStatusQuo(Identifier identifier);
-    BattleActor createWithFlatLevelFullHealth(Identifier identifier, int level);
-}
