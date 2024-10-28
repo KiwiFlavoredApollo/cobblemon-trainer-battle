25f8c7c309d6c31e51b597f08a1f06aa3f1fa2cd
fractalbarista <fractalbarista@gmail.com>
2024-10-28 23:21:32 +0900

    added advancements for inclement emerald gym leaders

--- src/main/java/kiwiapollo/cobblemontrainerbattle/datagen/InclementEmeraldAdvancementFactory.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/datagen/InclementEmeraldAdvancementFactory.java
@@ -41,11 +41,107 @@ public class InclementEmeraldAdvancementFactory implements AdvancementFactory {
             )
             .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_brawly"));
 
+    private static final Advancement WATTSON = Advancement.Builder.createUntelemetered()
+            .parent(BRAWLY)
+            .criterion("defeat_wattson", new DefeatTrainerCriterion.Conditions(1)) // TODO
+            .display(
+                    Registries.ITEM.get(Identifier.of("cobblemon", "poke_ball")),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_wattson.title"),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_wattson.description"),
+                    DataGenerator.AdvancementProvider.BACKGROUND,
+                    AdvancementFrame.TASK,
+                    true,
+                    true,
+                    false
+            )
+            .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_wattson"));
+
+    private static final Advancement FLANNERY = Advancement.Builder.createUntelemetered()
+            .parent(WATTSON)
+            .criterion("defeat_flannery", new DefeatTrainerCriterion.Conditions(1)) // TODO
+            .display(
+                    Registries.ITEM.get(Identifier.of("cobblemon", "poke_ball")),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_flannery.title"),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_flannery.description"),
+                    DataGenerator.AdvancementProvider.BACKGROUND,
+                    AdvancementFrame.TASK,
+                    true,
+                    true,
+                    false
+            )
+            .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_flannery"));
+
+    private static final Advancement NORMAN = Advancement.Builder.createUntelemetered()
+            .parent(FLANNERY)
+            .criterion("defeat_norman", new DefeatTrainerCriterion.Conditions(1)) // TODO
+            .display(
+                    Registries.ITEM.get(Identifier.of("cobblemon", "poke_ball")),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_norman.title"),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_norman.description"),
+                    DataGenerator.AdvancementProvider.BACKGROUND,
+                    AdvancementFrame.TASK,
+                    true,
+                    true,
+                    false
+            )
+            .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_norman"));
+
+    private static final Advancement WINONA = Advancement.Builder.createUntelemetered()
+            .parent(NORMAN)
+            .criterion("defeat_winona", new DefeatTrainerCriterion.Conditions(1)) // TODO
+            .display(
+                    Registries.ITEM.get(Identifier.of("cobblemon", "poke_ball")),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_winona.title"),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_winona.description"),
+                    DataGenerator.AdvancementProvider.BACKGROUND,
+                    AdvancementFrame.TASK,
+                    true,
+                    true,
+                    false
+            )
+            .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_winona"));
+
+    private static final Advancement TATE_AND_LIZA = Advancement.Builder.createUntelemetered()
+            .parent(WINONA)
+            .criterion("defeat_tate_and_liza", new DefeatTrainerCriterion.Conditions(1)) // TODO
+            .display(
+                    Registries.ITEM.get(Identifier.of("cobblemon", "poke_ball")),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_tate_and_liza.title"),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_tate_and_liza.description"),
+                    DataGenerator.AdvancementProvider.BACKGROUND,
+                    AdvancementFrame.TASK,
+                    true,
+                    true,
+                    false
+            )
+            .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_tate_and_liza"));
+
+    private static final Advancement JUAN = Advancement.Builder.createUntelemetered()
+            .parent(TATE_AND_LIZA)
+            .criterion("defeat_juan", new DefeatTrainerCriterion.Conditions(1)) // TODO
+            .display(
+                    Registries.ITEM.get(Identifier.of("cobblemon", "poke_ball")),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_juan.title"),
+                    Text.translatable("advancement.cobblemontrainerbattle.defeat_juan.description"),
+                    DataGenerator.AdvancementProvider.BACKGROUND,
+                    AdvancementFrame.TASK,
+                    true,
+                    true,
+                    false
+            )
+            .build(Identifier.of(CobblemonTrainerBattle.NAMESPACE, "defeat_juan"));
+
     @Override
     public List<Advancement> create() {
         return List.of(
                 ROXANNE,
-                BRAWLY
+                BRAWLY,
+                WATTSON,
+                FLANNERY,
+                NORMAN,
+                WINONA,
+                TATE_AND_LIZA,
+                JUAN
         );
     }
 }
