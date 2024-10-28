f65e05ce981a9b3b41691b1d9c1e4ebea223ac78
fractalbarista <fractalbarista@gmail.com>
2024-10-28 22:47:13 +0900

    added and registered inclement emerald gym leader tokens and tickets

--- src/main/java/kiwiapollo/cobblemontrainerbattle/item/ItemRegistry.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/item/ItemRegistry.java
@@ -17,6 +17,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ItemRegistry {
+
     public void register() {
         Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "trainer_spawn_egg"), TRAINER_SPAWN_EGG);
 
@@ -31,6 +32,24 @@ public class ItemRegistry {
 
         Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "radicalred_leader_brock_ticket"), RADICALRED_LEADER_BROCK_TICKET);
 
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_roxanne_token"), INCLEMENTEMERALD_LEADER_ROXANNE_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_brawly_token"), INCLEMENTEMERALD_LEADER_BRAWLY_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_wattson_token"), INCLEMENTEMERALD_LEADER_WATTSON_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_flannery_token"), INCLEMENTEMERALD_LEADER_FLANNERY_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_norman_token"), INCLEMENTEMERALD_LEADER_NORMAN_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_winona_token"), INCLEMENTEMERALD_LEADER_WINONA_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_tate_and_liza_token"), INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TOKEN);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_juan_token"), INCLEMENTEMERALD_LEADER_JUAN_TOKEN);
+
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_roxanne_ticket"), INCLEMENTEMERALD_LEADER_ROXANNE_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_brawly_ticket"), INCLEMENTEMERALD_LEADER_BRAWLY_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_wattson_ticket"), INCLEMENTEMERALD_LEADER_WATTSON_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_flannery_ticket"), INCLEMENTEMERALD_LEADER_FLANNERY_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_norman_ticket"), INCLEMENTEMERALD_LEADER_NORMAN_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_winona_ticket"), INCLEMENTEMERALD_LEADER_WINONA_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_tate_and_liza_ticket"), INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TICKET);
+        Registry.register(Registries.ITEM, Identifier.of(CobblemonTrainerBattle.NAMESPACE, "inclementemerald_leader_juan_ticket"), INCLEMENTEMERALD_LEADER_JUAN_TICKET);
+        
         Registry.register(Registries.ITEM_GROUP, ITEM_GROUP_KEY, ITEM_GROUP);
 
         ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP_KEY).register(itemGroup -> {
@@ -46,6 +65,24 @@ public class ItemRegistry {
             itemGroup.add(RADICALRED_LEADER_BROCK_TOKEN);
 
             itemGroup.add(RADICALRED_LEADER_BROCK_TICKET);
+            
+            itemGroup.add(INCLEMENTEMERALD_LEADER_ROXANNE_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_BRAWLY_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_WATTSON_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_FLANNERY_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_NORMAN_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_WINONA_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TOKEN);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_JUAN_TOKEN);
+
+            itemGroup.add(INCLEMENTEMERALD_LEADER_ROXANNE_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_BRAWLY_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_WATTSON_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_FLANNERY_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_NORMAN_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_WINONA_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TICKET);
+            itemGroup.add(INCLEMENTEMERALD_LEADER_JUAN_TICKET);
         });
     }
 
@@ -70,6 +107,73 @@ public class ItemRegistry {
             new SelectedTrainerEntityFactory(
                     Identifier.of("trainer", "radicalred/leader_brock"),
                     Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            )
+    );
+
+    public static final Item INCLEMENTEMERALD_LEADER_ROXANNE_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_BRAWLY_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_WATTSON_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_FLANNERY_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_NORMAN_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_WINONA_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TOKEN = new Item(new Item.Settings());
+    public static final Item INCLEMENTEMERALD_LEADER_JUAN_TOKEN = new Item(new Item.Settings());
+
+
+    public static final Item INCLEMENTEMERALD_LEADER_ROXANNE_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/roxanne_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_BRAWLY_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/brawly_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_WATTSON_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/wattson_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_FLANNERY_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/flannery_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_NORMAN_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/norman_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_WINONA_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/winona_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/tate_and_liza_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            )
+    );
+    public static final Item INCLEMENTEMERALD_LEADER_JUAN_TICKET = new TrainerTicket(
+            new Item.Settings(),
+            new SelectedTrainerEntityFactory(
+                    Identifier.of("trainer", "inclementemerald/juan_2"),
+                    Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
             )
     );
 }
