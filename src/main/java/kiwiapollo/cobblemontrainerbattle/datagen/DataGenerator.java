497782d38da8a4f0d3e740860d9a99c93ad8691f
fractalbarista <fractalbarista@gmail.com>
2024-10-29 16:29:34 +0900

    added recipe data generation removed deprecated recipes

--- src/main/java/kiwiapollo/cobblemontrainerbattle/datagen/DataGenerator.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/datagen/DataGenerator.java
@@ -1,17 +1,24 @@
 package kiwiapollo.cobblemontrainerbattle.datagen;
 
+import com.cobblemon.mod.common.CobblemonItems;
 import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
 import kiwiapollo.cobblemontrainerbattle.entities.EntityTypes;
+import kiwiapollo.cobblemontrainerbattle.item.ItemRegistry;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementFrame;
 import net.minecraft.advancement.criterion.PlayerInteractedWithEntityCriterion;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
 import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.predicate.item.ItemPredicate;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -24,6 +31,7 @@ public class DataGenerator implements DataGeneratorEntrypoint {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
         pack.addProvider(AdvancementProvider::new);
+        pack.addProvider(RecipeProvider::new);
     }
 
     static class AdvancementProvider extends FabricAdvancementProvider {
@@ -68,6 +76,79 @@ public class DataGenerator implements DataGeneratorEntrypoint {
             for (Advancement advancement : new InclementEmeraldAdvancementFactory().create()) {
                 consumer.accept(advancement);
             }
+        }
+    }
+
+    private static class RecipeProvider extends FabricRecipeProvider {
+        private RecipeProvider(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generate(Consumer<RecipeJsonProvider> exporter) {
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_ROXANNE_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.ROCK_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.ROCK_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.ROCK_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_BRAWLY_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.FIGHTING_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.FIGHTING_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.FIGHTING_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_WATTSON_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.ELECTRIC_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.ELECTRIC_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.ELECTRIC_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_FLANNERY_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.FIRE_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.FIRE_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.FIRE_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_NORMAN_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.NORMAL_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.NORMAL_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.NORMAL_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_WINONA_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.FLYING_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.FLYING_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.FLYING_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_TATE_AND_LIZA_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.PSYCHIC_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.PSYCHIC_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.PSYCHIC_GEM))
+                    .offerTo(exporter);
+
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.INCLEMENTEMERALD_LEADER_JUAN_TICKET)
+                    .input(ItemRegistry.TRAINER_TOKEN, 6)
+                    .input(Items.GREEN_DYE)
+                    .input(CobblemonItems.WATER_GEM)
+                    .criterion(FabricRecipeProvider.hasItem(ItemRegistry.TRAINER_TOKEN), FabricRecipeProvider.conditionsFromItem(ItemRegistry.TRAINER_TOKEN))
+                    .criterion(FabricRecipeProvider.hasItem(CobblemonItems.WATER_GEM), FabricRecipeProvider.conditionsFromItem(CobblemonItems.WATER_GEM))
+                    .offerTo(exporter);
         }
     }
 }
