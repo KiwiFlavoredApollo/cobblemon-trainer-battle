939c01babdfb06c13b65e99b7c9a4192bab8fb20
fractalbarista <fractalbarista@gmail.com>
2024-10-30 01:52:39 +0900

    edited inclement emerald gym leader presets, using separate team file

--- src/main/java/kiwiapollo/cobblemontrainerbattle/entities/InclementEmeraldTrainerEntityPresetRegistry.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/entities/InclementEmeraldTrainerEntityPresetRegistry.java
@@ -4,35 +4,35 @@ import net.minecraft.util.Identifier;
 
 public class InclementEmeraldTrainerEntityPresetRegistry {
     public static final TrainerEntityPreset LEADER_ROXANNE = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/roxanne_1"),
+            Identifier.of("trainer", "entity/leader_roxanne"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_BRAWLY = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/brawly_1"),
+            Identifier.of("trainer", "entity/leader_brawly"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_WATTSON = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/wattson_1"),
+            Identifier.of("trainer", "entity/leader_wattson"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_FLANNERY = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/flannery_1"),
+            Identifier.of("trainer", "entity/leader_flannery"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_NORMAN = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/norman_1"),
+            Identifier.of("trainer", "entity/leader_norman"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_WINONA = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/winona_1"),
+            Identifier.of("trainer", "entity/leader_winona"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_TATE_AND_LIZA = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/tate_and_liza_1"),
+            Identifier.of("trainer", "entity/leader_tate_and_liza"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
     public static final TrainerEntityPreset LEADER_JUAN = new TrainerEntityPreset(
-            Identifier.of("trainer", "inclementemerald/juan_1"),
+            Identifier.of("trainer", "entity/leader_juan"),
             Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
     );
 }
