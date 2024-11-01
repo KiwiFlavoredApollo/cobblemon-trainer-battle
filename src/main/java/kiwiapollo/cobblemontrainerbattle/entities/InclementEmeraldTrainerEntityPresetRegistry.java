8f2d2b97a0afa499b7ba523cb90e048b550bbbf4
fractalbarista <fractalbarista@gmail.com>
2024-11-01 10:20:04 +0900

    Added dummy textures for each TrainerEntityPreset, which later can be overridden by server admins

--- src/main/java/kiwiapollo/cobblemontrainerbattle/entities/InclementEmeraldTrainerEntityPresetRegistry.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/entities/InclementEmeraldTrainerEntityPresetRegistry.java
@@ -1,60 +1,61 @@
 package kiwiapollo.cobblemontrainerbattle.entities;
 
+import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
 import net.minecraft.util.Identifier;
 
 public class InclementEmeraldTrainerEntityPresetRegistry {
     public static final TrainerEntityPreset LEADER_ROXANNE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_roxanne"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_roxanne.png")
     );
     public static final TrainerEntityPreset LEADER_BRAWLY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_brawly"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_brawly.png")
     );
     public static final TrainerEntityPreset LEADER_WATTSON = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_wattson"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_wattson.png")
     );
     public static final TrainerEntityPreset LEADER_FLANNERY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_flannery"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_flannery.png")
     );
     public static final TrainerEntityPreset LEADER_NORMAN = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_norman"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_norman.png")
     );
     public static final TrainerEntityPreset LEADER_WINONA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_winona"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_winona.png")
     );
     public static final TrainerEntityPreset LEADER_TATE_AND_LIZA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_tate_and_liza"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_tate_and_liza.png")
     );
     public static final TrainerEntityPreset LEADER_JUAN = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_juan"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_juan.png")
     );
 
     public static final TrainerEntityPreset ELITE_SIDNEY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_sidney"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_sidney.png")
     );
     public static final TrainerEntityPreset ELITE_PHOEBE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_phoebe"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_phoebe.png")
     );
     public static final TrainerEntityPreset ELITE_GLACIA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_glacia"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_glacia.png")
     );
     public static final TrainerEntityPreset ELITE_DRAKE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_drake"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_drake.png")
     );
 
     public static final TrainerEntityPreset CHAMPION_WALLACE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/champion_wallace"),
-            Identifier.of("minecraft", "textures/entity/player/slim/alex.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/champion_wallace.png")
     );
 }
--- src/main/java/kiwiapollo/cobblemontrainerbattle/entities/RadicalRedTrainerEntityPresetRegistry.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/entities/RadicalRedTrainerEntityPresetRegistry.java
@@ -1,93 +1,94 @@
 package kiwiapollo.cobblemontrainerbattle.entities;
 
+import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
 import net.minecraft.util.Identifier;
 
 public class RadicalRedTrainerEntityPresetRegistry {
     public static final TrainerEntityPreset LEADER_BROCK = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_brock"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_brock.png")
     );
     public static final TrainerEntityPreset LEADER_MISTY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_misty"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_misty.png")
     );
     public static final TrainerEntityPreset LEADER_LT_SURGE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_lt_surge"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_lt_surge.png")
     );
     public static final TrainerEntityPreset LEADER_ERIKA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_erika"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_erika.png")
     );
     public static final TrainerEntityPreset LEADER_KOGA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_koga"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_koga.png")
     );
     public static final TrainerEntityPreset LEADER_SABRINA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_sabrina"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_sabrina.png")
     );
     public static final TrainerEntityPreset LEADER_BLAINE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_blaine"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_blaine.png")
     );
     public static final TrainerEntityPreset LEADER_GIOVANNI = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_giovanni"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_giovanni.png")
     );
 
     public static final TrainerEntityPreset LEADER_FALKNER = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_falkner"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_falkner.png")
     );
     public static final TrainerEntityPreset LEADER_BUGSY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_bugsy"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_bugsy.png")
     );
     public static final TrainerEntityPreset LEADER_WHITNEY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_whitney"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_whitney.png")
     );
     public static final TrainerEntityPreset LEADER_MORTY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_morty"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_morty.png")
     );
     public static final TrainerEntityPreset LEADER_CHUCK = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_chuck"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_chuck.png")
     );
     public static final TrainerEntityPreset LEADER_JASMINE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_jasmine"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_jasmine.png")
     );
     public static final TrainerEntityPreset LEADER_PRYCE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_pryce"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_pryce.png")
     );
     public static final TrainerEntityPreset LEADER_CLAIR = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/leader_clair"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/leader_clair.png")
     );
 
     public static final TrainerEntityPreset ELITE_LORELEI = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_lorelei"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_lorelei.png")
     );
     public static final TrainerEntityPreset ELITE_BRUNO = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_bruno"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_bruno.png")
     );
     public static final TrainerEntityPreset ELITE_AGATHA = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_agatha"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_agatha.png")
     );
     public static final TrainerEntityPreset ELITE_LANCE = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/elite_lance"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/elite_lance.png")
     );
 
     public static final TrainerEntityPreset CHAMPION_TERRY = new TrainerEntityPreset(
             Identifier.of("trainer", "entity/champion_terry"),
-            Identifier.of("minecraft", "textures/entity/player/slim/steve.png")
+            Identifier.of(CobblemonTrainerBattle.MOD_ID, "textures/entity/trainer/slim/champion_terry.png")
     );
 }
