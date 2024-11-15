b63ef2005ef50465db5cc5559d94ba303c39c2cf
fractalbarista <fractalbarista@gmail.com>
2024-11-15 16:55:29 +0900

    Added TrainerBattleRegistry

---
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/trainerbattle/TrainerBattleRegistry.java
@@ -0,0 +1,30 @@
+package kiwiapollo.cobblemontrainerbattle.trainerbattle;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class TrainerBattleRegistry {
+    private static final Map<UUID, TrainerBattle> battles = new HashMap<>();
+
+    public TrainerBattle get(UUID uuid) {
+        return battles.get(uuid);
+    }
+
+    public void put(UUID uuid, TrainerBattle trainerBattle) {
+        battles.put(uuid, trainerBattle);
+    }
+
+    public boolean containsKey(UUID uuid) {
+        return battles.containsKey(uuid);
+    }
+
+    public void remove(UUID uuid) {
+        battles.remove(uuid);
+    }
+
+    public Collection<TrainerBattle> values() {
+        return battles.values();
+    }
+}
--- src/main/java/kiwiapollo/cobblemontrainerbattle/trainerbattle/TrainerBattleStorage.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/trainerbattle/TrainerBattleStorage.java
@@ -4,42 +4,21 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 public class TrainerBattleStorage {
-    private static Map<UUID, TrainerBattle> battles = new HashMap<>();
+    private static final TrainerBattleRegistry storage = new TrainerBattleRegistry();
 
-    public static void put(UUID uuid, TrainerBattle trainerBattle) {
-        TrainerBattleStorage.battles.put(uuid, trainerBattle);
-    }
-
-    public static TrainerBattle get(UUID uuid) {
-        return TrainerBattleStorage.battles.get(uuid);
-    }
-
-    public static boolean containsKey(UUID uuid) {
-        return TrainerBattleStorage.battles.containsKey(uuid);
-    }
-
-    public static void remove(UUID uuid) {
-        TrainerBattleStorage.battles.remove(uuid);
-    }
-
-    public static Collection<TrainerBattle> values() {
-        return TrainerBattleStorage.battles.values();
+    public static TrainerBattleRegistry getTrainerBattleRegistry() {
+        return storage;
     }
 
     public static void removeDisconnectedPlayerBattle(ServerPlayNetworkHandler handler, MinecraftServer server) {
         ServerPlayerEntity player = handler.getPlayer();
 
-        if (!battles.containsKey(player.getUuid())) {
+        if (!storage.containsKey(player.getUuid())) {
             return;
         }
 
-        battles.get(player.getUuid()).onPlayerDefeat();
-        battles.remove(player.getUuid());
+        storage.get(player.getUuid()).onPlayerDefeat();
+        storage.remove(player.getUuid());
     }
 }
