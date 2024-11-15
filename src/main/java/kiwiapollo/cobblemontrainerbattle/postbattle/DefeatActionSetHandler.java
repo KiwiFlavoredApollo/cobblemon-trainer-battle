b51d6cf42832605b5d33423acbadf54ace3e818e
fractalbarista <fractalbarista@gmail.com>
2024-11-15 16:55:53 +0900

    Added CommandExecutor, DefeatActionSetHandler and VictoryActionSetHandler

---
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/postbattle/DefeatActionSetHandler.java
@@ -0,0 +1,21 @@
+package kiwiapollo.cobblemontrainerbattle.postbattle;
+
+import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class DefeatActionSetHandler implements Runnable {
+    private final ServerPlayerEntity player;
+    private final PostBattleActionSet onDefeat;
+
+    public DefeatActionSetHandler(ServerPlayerEntity player, PostBattleActionSet onDefeat) {
+        this.player = player;
+        this.onDefeat = onDefeat;
+    }
+
+    @Override
+    public void run() {
+        CobblemonTrainerBattle.economy.addBalance(player, onDefeat.balance);
+
+        onDefeat.commands.forEach(command -> new CommandExecutor().accept(player, command));
+    }
+}
---
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/postbattle/VictoryActionSetHandler.java
@@ -0,0 +1,21 @@
+package kiwiapollo.cobblemontrainerbattle.postbattle;
+
+import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class VictoryActionSetHandler implements Runnable {
+    private final ServerPlayerEntity player;
+    private final PostBattleActionSet onVictory;
+
+    public VictoryActionSetHandler(ServerPlayerEntity player, PostBattleActionSet onVictory) {
+        this.player = player;
+        this.onVictory = onVictory;
+    }
+
+    @Override
+    public void run() {
+        CobblemonTrainerBattle.economy.addBalance(player, onVictory.balance);
+
+        onVictory.commands.forEach(command -> new CommandExecutor().accept(player, command));
+    }
+}
