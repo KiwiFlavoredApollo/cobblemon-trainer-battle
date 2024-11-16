0910fd1dde93ecc672ceb4dc4a91e67716a2da29
fractalbarista <fractalbarista@gmail.com>
2024-11-16 17:27:34 +0900

    Edited HeldItemDeserializer to throw exceptions rather than returning null

--- src/main/java/kiwiapollo/cobblemontrainerbattle/parser/profile/HeldItemDeserializer.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/parser/profile/HeldItemDeserializer.java
@@ -3,21 +3,21 @@ package kiwiapollo.cobblemontrainerbattle.parser.profile;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 import java.lang.reflect.Type;
-import java.util.Objects;
+import java.util.NoSuchElementException;
 
 public class HeldItemDeserializer implements JsonDeserializer<ItemStack> {
     @Override
     public ItemStack deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
         try {
-            Identifier item = Objects.requireNonNull(Identifier.tryParse(json.getAsString()));
-            return new ItemStack(Registries.ITEM.get(item));
-        } catch (NullPointerException e) {
-            return null;
+            return new ItemStack(Registries.ITEM.getOrEmpty(Identifier.tryParse(json.getAsString())).get());
+        } catch (NoSuchElementException e) {
+            throw new JsonParseException(e);
         }
     }
 }
