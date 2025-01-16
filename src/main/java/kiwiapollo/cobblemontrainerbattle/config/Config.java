f8f3bf8f11a3995a8359b52450e0f2f44ad6caf0
fractalbarista <fractalbarista@gmail.com>
2025-01-16 16:26:04 +0900

    Edited Config

--- src/main/java/kiwiapollo/cobblemontrainerbattle/config/Config.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/config/Config.java
@@ -1,5 +1,11 @@
 package kiwiapollo.cobblemontrainerbattle.config;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Config {
+    @SerializedName("maximum_trainer_spawn_count")
     public int maximumTrainerSpawnCount;
+
+    @SerializedName("spawn_interval_in_seconds")
+    public int spawnIntervalInSeconds;
 }
