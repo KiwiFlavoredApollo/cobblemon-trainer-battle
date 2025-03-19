b768d80019167f22f2289a517aaaa6674efcc33e
fractalbarista <fractalbarista@gmail.com>
2025-03-19 14:26:41 +0900

    Added sound events for gym leaders, elite fours and champions

--- src/main/java/kiwiapollo/cobblemontrainerbattle/sound/BattleSounds.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/sound/BattleSounds.java
@@ -5,21 +5,77 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 public enum BattleSounds {
-    DEFAULT_DISC_1("battle.default.disc_1"),
-    DEFAULT_DISC_2("battle.default.disc_2"),
-    DEFAULT_DISC_3("battle.default.disc_3"),
+    LEADER_DEFAULT("battle.leader.default"),
+    ELITE_DEFAULT("battle.elite.default"),
+    CHAMPION_DEFAULT("battle.champion.default"),
 
-    GYM_LEADER_DISC_1("battle.gym_leader.disc_1"),
-    GYM_LEADER_DISC_2("battle.gym_leader.disc_2"),
-    GYM_LEADER_DISC_3("battle.gym_leader.disc_3"),
+    KANTO_DEFAULT("battle.kanto.default"),
+    JOHTO_DEFAULT("battle.johto.default"),
+    HOENN_DEFAULT("battle.hoenn.default"),
+    SINNOH_DEFAULT("battle.sinnoh.default"),
+    UNOVA_DEFAULT("battle.unova.default"),
+    KALOS_DEFAULT("battle.kalos.default"),
+    ALOLA_DEFAULT("battle.alola.default"),
+    GALAR_DEFAULT("battle.galar.default"),
+    HISUI_DEFAULT("battle.hisui.default"),
+    PALDEA_DEFAULT("battle.paldea.default"),
 
-    ELITE_FOUR_DISC_1("battle.elite_four.disc_1"),
-    ELITE_FOUR_DISC_2("battle.elite_four.disc_2"),
-    ELITE_FOUR_DISC_3("battle.elite_four.disc_3"),
+    LEADER_BROCK("battle.leader.brock"),
+    LEADER_MISTY("battle.leader.misty"),
+    LEADER_LT_SURGE("battle.leader.lt_surge"),
+    LEADER_ERIKA("battle.leader.erika"),
+    LEADER_KOGA("battle.leader.koga"),
+    LEADER_SABRINA("battle.leader.sabrina"),
+    LEADER_BLAINE("battle.leader.blaine"),
+    LEADER_GIOVANNI("battle.leader.giovanni"),
 
-    CHAMPION_DISC_1("battle.champion.disc_1"),
-    CHAMPION_DISC_2("battle.champion.disc_2"),
-    CHAMPION_DISC_3("battle.champion.disc_3");
+    LEADER_FALKNER("battle.leader.falkner"),
+    LEADER_BUGSY("battle.leader.bugsy"),
+    LEADER_WHITNEY("battle.leader.whitney"),
+    LEADER_MORTY("battle.leader.morty"),
+    LEADER_CHUCK("battle.leader.chuck"),
+    LEADER_JASMINE("battle.leader.jasmine"),
+    LEADER_PRYCE("battle.leader.pryce"),
+    LEADER_CLAIR("battle.leader.clair"),
+
+    ELITE_LORELEI("battle.elite.lorelei"),
+    ELITE_BRUNO("battle.elite.bruno"),
+    ELITE_AGATHA("battle.elite.agatha"),
+    ELITE_LANCE("battle.elite.lance"),
+
+    CHAMPION_TERRY("battle.champion.terry"),
+
+    LEADER_ROXANNE("battle.leader.roxanne"),
+    LEADER_BRAWLY("battle.leader.brawly"),
+    LEADER_WATTSON("battle.leader.wattson"),
+    LEADER_FLANNERY("battle.leader.flannery"),
+    LEADER_NORMAN("battle.leader.norman"),
+    LEADER_WINONA("battle.leader.winona"),
+    LEADER_TATE_AND_LIZA("battle.leader.tate_and_liza"),
+    LEADER_JUAN("battle.leader.juan"),
+
+    ELITE_SIDNEY("battle.elite.sidney"),
+    ELITE_PHOEBE("battle.elite.phoebe"),
+    ELITE_GLACIA("battle.elite.glacia"),
+    ELITE_DRAKE("battle.elite.drake"),
+
+    CHAMPION_WALLACE("battle.champion.wallace"),
+
+    LEADER_ROARK("battle.leader.roark"),
+    LEADER_GARDENIA("battle.leader.gardenia"),
+    LEADER_MAYLENE("battle.leader.maylene"),
+    LEADER_CRASHER_WAKE("battle.leader.crasher_wake"),
+    LEADER_FANTINA("battle.leader.fantina"),
+    LEADER_BYRON("battle.leader.byron"),
+    LEADER_CANDICE("battle.leader.candice"),
+    LEADER_VOLKNER("battle.leader.volkner"),
+
+    ELITE_AARON("battle.elite.aaron"),
+    ELITE_BERTHA("battle.elite.bertha"),
+    ELITE_FLINT("battle.elite.flint"),
+    ELITE_LUCIAN("battle.elite.lucian"),
+
+    CHAMPION_CYNTHIA("battle.champion.cynthia");
 
     private final Identifier identifier;
 
