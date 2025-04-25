92c8c501658b23c5169c2517168ae65ff7cc13b1
fractalbarista <fractalbarista@gmail.com>
2025-04-25 15:50:25 +0900

    Removed unused classes

--- src/main/java/kiwiapollo/cobblemontrainerbattle/entity/TrainerEntityPackets.java
+++
@@ -1,8 +0,0 @@
-package kiwiapollo.cobblemontrainerbattle.entity;
-
-import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
-import net.minecraft.util.Identifier;
-
-public class TrainerEntityPackets {
-    public static final Identifier TRAINER_ENTITY_SYNC = Identifier.of(CobblemonTrainerBattle.MOD_ID, "trainer_entity_sync");
-}
