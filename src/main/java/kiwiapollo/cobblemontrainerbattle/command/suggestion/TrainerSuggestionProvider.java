f579b07532496b0f861634a9daced8b6e0bb57a4
fractalbarista <fractalbarista@gmail.com>
2025-11-25 00:44:25 +0900

    Edited TrainerSuggestionProvider to add different suggestion depending on namespaces

--- src/main/java/kiwiapollo/cobblemontrainerbattle/command/suggestion/TrainerSuggestionProvider.java
+++ src/main/java/kiwiapollo/cobblemontrainerbattle/command/suggestion/TrainerSuggestionProvider.java
@@ -5,20 +5,31 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import kiwiapollo.cobblemontrainerbattle.CobblemonTrainerBattle;
 import kiwiapollo.cobblemontrainerbattle.global.preset.TrainerTemplateStorage;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class TrainerSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
     @Override
     public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
         String input = getIncompleteInput(context);
-        TrainerTemplateStorage.getInstance().keySet().stream().filter(identifier -> identifier.toString().startsWith(input)).map(Identifier::toString).forEach(builder::suggest);
-        TrainerTemplateStorage.getInstance().keySet().stream().filter(identifier -> identifier.getPath().startsWith(input)).map(Identifier::toString).forEach(builder::suggest);
+
+        TrainerTemplateStorage.getInstance().keySet().stream()
+                .filter(identifier -> !Objects.equals(identifier.getNamespace(), CobblemonTrainerBattle.MOD_ID))
+                .filter(identifier -> identifier.toString().startsWith(input))
+                .map(Identifier::toString).forEach(builder::suggest);
+
+        TrainerTemplateStorage.getInstance().keySet().stream()
+                .filter(identifier -> Objects.equals(identifier.getNamespace(), CobblemonTrainerBattle.MOD_ID))
+                .filter(identifier -> identifier.getPath().startsWith(input))
+                .map(Identifier::toString).forEach(builder::suggest);
+
         return builder.buildFuture();
     }
 
