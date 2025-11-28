306c31af4ab660b0960ea2598a07e82ad4eedad0
fractalbarista <fractalbarista@gmail.com>
2025-11-29 02:19:52 +0900

    Removed never used Villager Profession

--- src/main/java/kiwiapollo/cobblemontrainerbattle/villager/TrainerReceptionistVillager.java
+++
@@ -1,6 +0,0 @@
-package kiwiapollo.cobblemontrainerbattle.villager;
-
-public class TrainerReceptionistVillager {
-    public static final String POI_ID = "trainerreceptionist_poi";
-    public static final String PROFESSION_ID = "trainerreceptionist";
-}
